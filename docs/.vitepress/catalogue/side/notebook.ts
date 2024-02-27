588481c19c068d78e5fc29f0293332c151036c74
GengZzz <1362284172@qq.com>
2024-02-27 17:20:10 +0800

    添加随笔中的分类目录，添加博客《vitepress项目部署到github Pages》

--- docs/.vitepress/catalogue/side/notebook.ts
+++ docs/.vitepress/catalogue/side/notebook.ts
@@ -1,9 +1,42 @@
 function sideNotebook(){
     return [
         {
-            text:'vitepress部署到github',
-            link:'/notebook/index'
-        }
+            text:'前端',
+            collapsed:false,
+            items:[
+                { text:'vitepress部署到github Pages',link:'/notebook/index'}
+            ]
+        },
+        {
+            text:'服务端',
+            collapsed:false,
+            items:[]
+        },
+        {
+            text:'框架',
+            collapsed:false,
+            items:[]
+        },
+        {
+            text:'开发工具',
+            collapsed:false,
+            items:[]
+        },
+        {
+            text:'数据库',
+            collapsed:false,
+            items:[]
+        },
+        {
+            text:'CI/CD',
+            collapsed:false,
+            items:[]
+        },
+        {
+            text:'其他',
+            collapsed:false,
+            items:[]
+        },
     ]
 }
 

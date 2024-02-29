f8442208401b5f03baf935eff74228a9f871dff4
GengZzz <1362284172@qq.com>
2024-03-01 01:41:19 +0800

    修改javascript大纲内容

--- docs/.vitepress/catalogue/side/javascript.ts
+++ docs/.vitepress/catalogue/side/javascript.ts
@@ -16,98 +16,77 @@ function sidebarJavaScript(){
             text: '运算符与流程控制',
             collapsed: false,  
             items: [
-                { text: '运算符', link: '/web/js/index.md' },
-                { text: '流程控制', link: '/web/js/index.md' },
+               
             ],
         },
         {
             text: '变量、作用域和内存问题',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: '基本类型',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: '数组',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: '函数',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+               
             ],
         },
         {
             text: 'Symbol',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: 'Set',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: 'Map',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: '函数表达式',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+               
             ],
         },
         {
             text: '面向对象的程序设计(对象)',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: '原型与继承',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         
@@ -115,135 +94,105 @@ function sidebarJavaScript(){
             text: '类',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: '模块设计',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+               
             ],
         },
         {
             text: '正则表达式',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: 'Promise',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+               
             ],
         },
         {
             text: '任务管理',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: 'BOM',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: '客户端检测',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: 'DOM',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+               
             ],
         },
         {
             text: '事件',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+                
             ],
         },
         {
             text: 'canvas',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+               
             ],
         },
         {
             text: '表单脚本',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+               
             ],
         },
         {
             text: 'HTML5脚本编程',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+               
             ],
         },
         {
             text: 'Ajax与Comet',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+               
             ],
         },
         {
             text: '离线应用于客户端存储',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+               
             ],
         },
         {
             text: '高级技巧',
             collapsed: false,  
             items: [
-                { text: '基本类型和引用类型的值', link: '/web/js/index.md' },
-                { text: '执行环境及作用域', link: '/web/js/other.md' },
-                { text: '垃圾收集', link: '/web/js/index.md' }
+               
             ],
         },
     ]
